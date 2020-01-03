848aa78f193495c2d7f1d90d35ea90a6d9f5f57e
Chris Umphlett <christopher.umphlett@gmail.com>
2020-01-03 09:52:11 -0500

    added overwrite function

--- WRITE_TO_SQL.R
+++ WRITE_TO_SQL.R
@@ -3,3 +3,9 @@ write_df_to_sql_append <- function(db_server, db_name, schema.tablename, df, var
   sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = TRUE, varTypes = vartypes)
   odbcClose(conn)
 }
+
+write_df_to_sql_overwrite <- function(db_server, db_name, schema.tablename, df, vartypes = NULL) {
+  conn <- odbcDriverConnect(paste0("driver={SQL Server}; server=", db_server, "; database=", db_name, "; trusted_connection = true"))
+  sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = FALSE, varTypes = vartypes)
+  odbcClose(conn)
+}
