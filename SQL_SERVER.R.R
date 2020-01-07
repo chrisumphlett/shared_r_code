bec78ffbb9aa39906422d2dffaff25e161ab21fa
Chris Umphlett <christopher.umphlett@gmail.com>
2020-01-07 13:46:07 -0500

    added qry_sql_server function. changed filename to more general to include read from or write to

--- SQL_SERVER.R.R
+++ SQL_SERVER.R.R
@@ -10,3 +10,10 @@ write_df_to_sql_overwrite <- function(db_server, db_name, schema.tablename, df, 
   sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = FALSE)
   odbcClose(conn)
 }
+
+query_sql_server <- function(db_server, db_name, query_text) {
+  conn <- odbcDriverConnect(paste0("driver={SQL Server}; server=", db_server, "; database=", db_name, "; trusted_connection = true"))
+  df <- sqlQuery(query_text, stringsAsFactors = FALSE)
+  odbcClose(conn)
+  return(df)
+}
