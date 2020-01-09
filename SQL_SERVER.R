5a15114744be3ac89dbd22d459bb5ea90870b8d3
Chris Umphlett <christopher.umphlett@gmail.com>
2020-01-09 09:12:52 -0500

    add library(RODBC)

--- SQL_SERVER.R
+++ SQL_SERVER.R
@@ -1,3 +1,4 @@
+library(RODBC)
 write_df_to_sql_append <- function(db_server, db_name, schema.tablename, df, vartypes = NULL) {
   conn <- odbcDriverConnect(paste0("driver={SQL Server}; server=", db_server, "; database=", db_name, "; trusted_connection = true"))
   sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = TRUE, varTypes = vartypes)
@@ -13,7 +14,7 @@ write_df_to_sql_overwrite <- function(db_server, db_name, schema.tablename, df, 
 
 query_sql_server <- function(db_server, db_name, query_text) {
   conn <- odbcDriverConnect(paste0("driver={SQL Server}; server=", db_server, "; database=", db_name, "; trusted_connection = true"))
-  df <- sqlQuery(query_text, stringsAsFactors = FALSE)
+  df <- sqlQuery(conn, query_text, stringsAsFactors = FALSE)
   odbcClose(conn)
   return(df)
 }
