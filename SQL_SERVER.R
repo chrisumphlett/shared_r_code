48876bc11e7dbdcd1aef2fd967f47871c85708b4
Chris Umphlett <christopher.umphlett@gmail.com>
2020-01-17 10:57:45 -0500

    added var types param to overwrite fn

--- SQL_SERVER.R
+++ SQL_SERVER.R
@@ -8,7 +8,7 @@ write_df_to_sql_append <- function(db_server, db_name, schema.tablename, df, var
 write_df_to_sql_overwrite <- function(db_server, db_name, schema.tablename, df, ...) {
   conn <- odbcDriverConnect(paste0("driver={SQL Server}; server=", db_server, "; database=", db_name, "; trusted_connection = true"))
   sqlDrop(conn, schema.tablename) 
-  sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = FALSE)
+  sqlSave(conn, df, tablename = schema.tablename, rownames = FALSE, append = FALSE, varTypes = vartypes)
   odbcClose(conn)
 }
 
